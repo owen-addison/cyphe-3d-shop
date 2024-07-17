1f5d31748429effbd8781fe8995892868862b0f9
Owen Lewis <owenalewis@gmail.com>
2024-07-17 11:56:56 +0200

    [fix] Add import statement for THREE namespace This import brings in the entire THREE namespace, which includes the Mesh type previously throwing an error.

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Box } from '@react-three/drei';
+import * as THREE from 'three';
 
 interface ItemData {
   id: number;
