c3b3f2892d024e6bef64e8719772f60d43070e0c
Owen Lewis <owenalewis@gmail.com>
2024-07-29 14:07:06 +0200

    [style] Remove text colour change on hover Removes the text colour change on hover for the "+" and "-" buttons of the ItemCounter. This improves the overall look and feel of the page.

--- src/components/ItemCounter.tsx
+++ src/components/ItemCounter.tsx
@@ -31,7 +31,7 @@ const ItemCounter: React.FC<ItemCounterProps> = ({
     <div className="flex items-center space-x-2">
       <button
         onClick={decrement}
-        className="font-mohave flex h-8 w-8 items-center justify-center text-2xl font-light text-moss-800 transition-all hover:text-3xl hover:text-moss-950"
+        className="font-mohave flex h-8 w-8 items-center justify-center text-2xl font-light text-moss-800 transition-all hover:text-3xl"
         aria-label="Decrease quantity"
       >
         -
@@ -41,7 +41,7 @@ const ItemCounter: React.FC<ItemCounterProps> = ({
       </span>
       <button
         onClick={increment}
-        className="font-mohave flex h-8 w-8 items-center justify-center text-2xl font-light text-moss-800 transition-all hover:text-3xl hover:text-moss-950"
+        className="font-mohave flex h-8 w-8 items-center justify-center text-2xl font-light text-moss-800 transition-all hover:text-3xl"
         aria-label="Increase quantity"
       >
         +
