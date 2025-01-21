d4fb004da92241304c75de061edb129397ff5cdb
Owen Lewis <owenalewis@gmail.com>
2025-01-21 11:14:57 +0100

    [feat] Add orbit controls - Add OrbitControls from drei to allow camera manipulation

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Model as SoapBar1 } from './SoapBar1';
+import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 // import { Box } from '@react-three/drei';
 import FloatingInfoPoint from './FloatingInfoPoint';
@@ -114,6 +115,7 @@ const Item: React.FC<ItemProps> = ({ data }) => {
           <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
             <ambientLight intensity={0.5} />
             <pointLight position={[10, 10, 10]} />
+            <OrbitControls />
             <SoapBar1 scale={30} />
           </Canvas>
         </div>
