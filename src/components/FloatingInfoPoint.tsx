269688e3ba6640a745af774bd995c8352d2537e7
Owen Lewis <owenalewis@gmail.com>
2025-03-13 11:16:31 +0100

    [fix] Change the z-index of FloatingInfoPoint
    - Changes the z-index of the FloatingInfoPoint float-container.
    - This results in the info point and ingredients being displayed over
      the top of the canvas element and 3D model.
    
    This change provides a cleaner display and easier to read ingredients.

--- src/components/FloatingInfoPoint.tsx
+++ src/components/FloatingInfoPoint.tsx
@@ -62,7 +62,7 @@ const FloatingInfoPoint: React.FC<FloatingInfoPointProps> = ({
   return (
     <div
       ref={containerRef}
-      className="float-container absolute h-40 w-52"
+      className="float-container absolute z-30 h-40 w-52"
       style={{ top: position.top, left: position.left }}
     >
       <motion.div
